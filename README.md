f997c3e17494ea824b7907fc74d684bbb2123f39
Cedric Van Bockhaven <cedric@ce3c.be>
2013-10-05 20:06:42 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
-portscan-async
+Asynchronous port scanner in PHP
 ==============
 
 An asynchronous port scanner in PHP
+
+```php
+$ss = new PortScan\Scanner;
+$ss->setTimeout(2);
+
+$ss->addTest('dante.yuso.be', 80);
+$ss->addTest('google.com', 80);
+$ss->addTest('1.2.3.4', 80);
+
+$ss->scan();
+var_dump($ss->results);
+```
+
+Keywords: multi threaded port scan status check checker scanner open closed
---
+++ src/Scanner.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace PortScan;
+
+class Scanner {
+	private $timeout = 5;
+	private $tests;
+	private $lookup;
+	private $socks;
+	public $results;
+
+	public function __construct() {
+	}
+
+	public function addTest($host, $port) {
+		$this->tests[] = [$host, $port];
+	}
+
+	// you can use a float
+	public function setTimeout($seconds) {
+		$this->timeout = $seconds;
+	}
+
+	private function createSockets() {
+		foreach ($this->tests as list($host, $port)) {
+			$sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+			socket_set_nonblock($sock);
+			@socket_connect($sock, $host, $port);
+
+			$this->socks[] = $sock;
+			$this->lookup[$sock] = [$host, $port];
+		}
+	}
+
+	private function handleSelect($selects, $func) {
+		foreach ($selects as $idx => $sock) {
+			list ($host, $port) = $this->lookup[$sock];
+			$this->results[$host][$port] = $func($sock);
+	
+			unset($this->socks[$idx]);
+			socket_close($sock);
+		}
+	}
+
+	public function scan($cb) {
+		$this->createSockets();
+
+		$poll = microtime(true);
+		while ($this->socks && microtime(true) - $poll < $this->timeout) {
+			$null = null;
+			$write = $this->socks;
+
+			socket_select($null, $write, $null, 0, 50);
+			$this->handleSelect($write, function($sock) {
+				return socket_get_option($sock, SOL_SOCKET, SO_ERROR);
+			});
+		}
+
+		$this->handleSelect($this->socks, function() { return SOCKET_ETIMEDOUT; });
+	}
+}
